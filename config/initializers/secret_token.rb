79518f1f38e3fb4ba0db239ed7df26a8d6ba1da9
David Ilizarov <dilizarov@gmail.com>
2013-09-22 21:37:48 -0500

    Boilerplate User Authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boilerplate::Application.config.secret_token = 'ccb41d07c85543d90e5b3e3d7c935ba1609e1200e09116c29f7dcf97041c0770dd339ef18d9295fa5bc38b971e87bcc3036351763a548bde8eafee413e8bf89b'
